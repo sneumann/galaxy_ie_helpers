378f0216882a279e35ceda880c2038147fdd6db9
Björn Grüning <bjoern@gruenings.eu>
2023-01-22 18:21:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = 'galaxy-ie-helpers',
     packages = ['galaxy_ie_helpers'],
-    version = '0.2.5',
+    version = '0.2.7',
     description = "Helper scripts to work with Galaxy's Interactive Environments",
     author = 'Bjoern A. Gruening; Helena Rasche',
     author_email = 'bjoern.gruening@gmail.com',
